a9c0b38a20051f8b432e0b84e773cf4556116e8c
Zahra A <zahraa@Zahras-Air.home>
2022-11-02 14:21:21 -0400

    working

---
+++ app.js
@@ -0,0 +1,11 @@
+// let someone message me
+// carousel with photos ?
+// let highlighted word initiate a photo modal or fun fact modal 
+
+// need footer at bottom with icon logos
+// delayed message upon screen loading would b cool
+
+
+$(() => {
+
+})
