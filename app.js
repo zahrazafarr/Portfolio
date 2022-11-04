6ed7bd436a710966c0252d8f3327c4f79af8a60b
Zahra A <zahraa@Zahras-Air.home>
2022-11-04 12:30:20 -0400

    sadface

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // let someone message me
 //carousel with photos ?
 // let highlighted word initiate a photo modal or fun fact modal 
+// find a way to open and close projects and experience section..... like wtf how did u forget.
 
 // need footer at bottom with icon logos
 
-
+// how do i delete my selfie on my git. would it interfere w final pushed html
 
 
 
 //////////////////////////////////////////////////////////////////////////
-// why doesnt beginning work when other stuff is entered in jquery
+// why doesnt this beginning work when other stuff is entered in jquery
 
 $(() => {
 
@@ -22,11 +23,12 @@ $('body').show().fadeIn(3000)
 
 // trying to get "message sent" log when submit clicked
 
-{/* <form class="message" action="" method="post">
+/* <form class="message" action="" method="post">
             Let's get in touch!
             <input type="text" placeholder="Enter your email">
             <input type="submit" class="button">
-        </form> */}
+        </form> */
+
  const submitForm() => {
     let $enter = $('.button');
     $enter.submit();
